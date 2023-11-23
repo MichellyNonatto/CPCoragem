91bc22b6246b1a7b81706c94fbc27bd15558b8ad
Michelly <smartnovo01@gmail.com>
2023-11-23 17:43:39 -0300

    Update: settings.py

--- coragem/settings.py
+++ coragem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w&v5g!ag2wqvf7quuz41mr0i-)*5l5%)hj+1ab6e(daw7jr*3+
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['*']
+#CSRF_TRUSTED_ORIGINS = ['*']
 CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
 
 # Application definition
