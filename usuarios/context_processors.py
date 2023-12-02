a3028149e4eed7350dbe251c2ade771037d56ce9
paulaandrezza <paulaandrezza25@gmail.com>
2023-12-02 17:20:13 -0300

    feat: Added context processor to provide employee information across all templates

---
+++ usuarios/context_processors.py
@@ -0,0 +1,13 @@
+from .models import Funcionario
+
+
+def funcionario(request):
+    if request.user.is_authenticated:
+        try:
+            funcionario = Funcionario.objects.get(usuario=request.user)
+        except Funcionario.DoesNotExist:
+            funcionario = None
+    else:
+        funcionario = None
+
+    return {'funcionario': funcionario}
