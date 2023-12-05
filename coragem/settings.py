a341d2b0b51a7908c63b1a924ecf80b8aeb3e813
Michelly Nonato <smartnovo01@gmail.com>
2023-12-05 15:27:12 -0300

    Config new db

--- coragem/settings.py
+++ coragem/settings.py
@@ -85,17 +85,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'BAc3*-f6B-gc*c35*ega1-G*-Dd-3c*b',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '55077',
+        'PASSWORD': '5bGAgAAC45Dfb5CbA*d11*1411ddf4dc',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '30659',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
